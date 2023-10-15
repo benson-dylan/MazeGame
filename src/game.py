dc350072653351dc62944563af00e4d377332270
benson-dylan <dylan222444@gmail.com>
2023-10-15 16:36:27 -0400

    Reworked game from ground up

---
+++ src/game.py
@@ -0,0 +1,115 @@
+import pygame as pg
+from OpenGL.GL import *
+import numpy as np
+import shaderLoaderV3 as sl
+import pyrr
+
+from utils import load_image
+
+class App:
+
+    def __init__(self):
+        
+        # Pygame Setup
+        pg.init()
+        pg.display.set_mode((640, 480), pg.OPENGL | pg.DOUBLEBUF)
+        self.clock = pg.time.Clock()
+        #Background Color
+        glClearColor(0.2, 0.5, 0.5, 1)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        self.shader = self.createShader("../assets/shaders/vertex.glsl", "../assets/shaders/fragment.glsl")
+        glUseProgram(self.shader.shader)
+        glUniform1i(glGetUniformLocation(self.shader.shader, "imageTexture"), 0)
+        self.triangle = Triangle()
+        self.sky_texture = Material("../assets/textures/Teefy.png")
+        self.mainLoop()
+
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        shader = sl.ShaderProgram(vertexFilepath, fragmentFilepath)
+        return shader 
+
+    def mainLoop(self):
+        
+        draw = True
+        while draw:
+            #Check events
+            for event in pg.event.get():
+                if (event.type == pg.QUIT):
+                    draw = False
+            #Refresh screen
+            glClear(GL_COLOR_BUFFER_BIT)
+
+            glUseProgram(self.shader.shader)
+            self.sky_texture.use()
+            glBindVertexArray(self.triangle.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.triangle.n_vertices)
+            pg.display.flip()
+
+            #Timing
+            self.clock.tick(60)
+        self.quit()
+
+    def quit(self):
+        self.triangle.destroy()
+        glDeleteProgram(self.shader.shader)
+        self.sky_texture.destroy()
+        pg.QUIT
+
+class Triangle:
+
+    def __init__(self):
+        
+        # x, y, z, r, g, b
+        self.vertices = (
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+             0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+             0.0,  0.5, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0
+        )
+
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+        self.n_vertices = 3
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(2)
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
+
+class Material:
+
+    def __init__(self, filepath):
+        
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        img_data, img_width, img_height = load_image(filepath)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img_width, img_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
+
+if __name__ == "__main__":
+    myApp = App()
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import pygame as pg
 def load_image(filename):
     img = pg.image.load(filename)
-    img_data = pg.image.tobytes(img, "RGB", True)
-    w, h = img.get_size()
+    img_data = pg.image.tostring(img, "RGBA")
+    w, h = img.get_rect().size
     return img_data, w, h
